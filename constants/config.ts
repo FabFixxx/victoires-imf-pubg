d8e1e4c78cf39ba27d53f438b2cba8c46add3bf5
FabFixxx <fwagner@divalto.com>
2026-06-13 23:37:39 +0200

    Env vars via EAS: config.ts reads process.env, secrets stored on EAS servers

---
+++ constants/config.ts
@@ -0,0 +1,6 @@
+﻿export const PUBG_API_KEY = process.env.EXPO_PUBLIC_PUBG_API_KEY ?? '';
+export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
+export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
+
+export const PUBG_SHARD = 'steam';
+export const PUBG_BASE_URL = `https://api.pubg.com/shards/${PUBG_SHARD}`;
